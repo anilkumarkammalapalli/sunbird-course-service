6944e07ee01db190f002ae1c68c134aeb024b20b
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-09-22 19:26:04 +0530

    hotfix: startDate format issue

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/util/BatchCacheHandlerV2.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/util/BatchCacheHandlerV2.java
@@ -12,7 +12,9 @@ import org.sunbird.common.models.util.PropertiesCache;
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.helper.ServiceFactory;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -76,6 +78,11 @@ public class BatchCacheHandlerV2 {
                     @SuppressWarnings("unchecked")
                     Map<String, Object> fetchedContent = (Map<String, Object>) responseList.get(0);
                     if (fetchedContent != null && !fetchedContent.isEmpty()) {
+                        if (fetchedContent.containsKey("startDate") && fetchedContent.get("startDate") instanceof Date) {
+                            Date startDate = (Date) fetchedContent.get("startDate");
+                            String formattedStartDate = new SimpleDateFormat("yyyy-MM-dd").format(startDate);
+                            fetchedContent.put("startDate", formattedStartDate);
+                        }
                         batchCache.put(batchId, fetchedContent);
                         return fetchedContent;
                     } else {
